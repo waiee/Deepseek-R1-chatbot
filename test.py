88aaf5c1b8d10815d4fac6b8d5ff0a1a7a114f2a
Waiee Zainol <69743222+waiee@users.noreply.github.com>
2024-11-19 21:46:02 +0800

    c

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
-print("hello anjing!")
-print("test commit")
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model = "HuggingFaceH4/starchat-beta"
+llm = HuggingFaceHub(repo_id=model ,
+                         model_kwargs={"min_length":30,
+                                       "max_new_tokens":500, "do_sample":True,
+                                       "temperature":0.2, "top_k":50,
+                                       "top_p":0.95, "eos_token_id":49155})
